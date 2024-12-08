b22c9f11c63bbbaecb09d2536f7cba4a7d7aaa53
SaeedKarimian860 <SGK12190@gmail.com>
2024-12-08 00:37:37 -0500

    Testing Framework + jsdom

--- render.js
+++ render.js
@@ -1,16 +1,20 @@
-const path = require('path');
-const jsdom = require('jsdom');
+const path = require("path");
+const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 const render = async (filename) => {
-    const filePath = path.join(process.cwd(), filename);
+  const filePath = path.join(process.cwd(), filename);
 
-    const dom = await JSDOM.fromFile(filePath, {
-        runScripts: 'dangerously',
-        resources: 'usable'
+  const dom = await JSDOM.fromFile(filePath, {
+    runScripts: "dangerously",
+    resources: "usable",
+  });
+
+  return new Promise((resolve, reject) => {
+    dom.window.document.addEventListener("DOMContentLoaded", () => {
+      resolve(dom);
     });
-
-    return dom;
-};  
+  });
+};
 
 module.exports = render;
